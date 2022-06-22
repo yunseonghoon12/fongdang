b3176e35fefae9a8bdc85fd18dcd36561d733da9
Hwangsunae88 <sunea1337@gmail.com>
2022-06-22 23:28:16 +0900

    src_maker  삭제
    Maker Controller외

--- fongdang_src/src/main/java/kh/spring/sunea/maker/model/service/MakerService.java
+++
@@ -1,9 +0,0 @@
-package kh.spring.sunea.maker.model.service;
-
-import kh.spring.sunea.maker.domain.Maker;
-
-public interface MakerService {
-	
-	/* 메이커등록 */
-	public int insertMaker(Maker maker);
-}
