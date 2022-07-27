b0af2befbef5d0aeee54dfc9242992aae2c2f3e7
iglove98 <98322180+iglove98@users.noreply.github.com>
2022-07-27 15:55:09 +0900

    deletePay

--- fongdang_src/src/main/java/kh/spring/fongdang/pay/model/service/PayService.java
+++
@@ -1,9 +0,0 @@
-package kh.spring.fongdang.pay.model.service;
-
-import kh.spring.fongdang.order.domain.Order;
-import kh.spring.fongdang.pay.domain.Pay;
-
-public interface PayService {
-
-	public Pay selectPay(Order order);
-}
