bcaaf353da62b97bd5a8ed3dba7e4918fcd8694d
yunseonghoon <freekey3@naver.com>
2022-08-01 22:00:40 +0900

    이용가이드 알림

--- fongdang_src/src/main/java/kh/spring/fongdang/guide/controller/GuideController.java
+++ fongdang_src/src/main/java/kh/spring/fongdang/guide/controller/GuideController.java
@@ -2,18 +2,31 @@ package kh.spring.fongdang.guide.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kh.spring.fongdang.alarm.model.service.AlarmServiceImpl;
+import kh.spring.fongdang.member.domain.Member;
+
 @Controller
 @RequestMapping("/guide")
 public class GuideController {
 	
+	@Autowired
+	private AlarmServiceImpl alarmservice;
 	
 	@RequestMapping(value = "/book", method = RequestMethod.GET)
 	public ModelAndView guide(ModelAndView mv, HttpSession session) {
+		
+		Member member = (Member) session.getAttribute("loginInfo");
+		if (member != null) {
+			String email = member.getEmail();
+			mv.addObject("alarm",alarmservice.countAlarm(email));
+			}
+		
 		mv.setViewName("customerCenter/guide");
 		return mv;
 	}
